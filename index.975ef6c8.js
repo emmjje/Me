5cec161bfee4ae2df68ba42af8d8871da3615d22
emmjje <119668591+emmjje@users.noreply.github.com>
2023-12-04 10:56:55 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27177,7 +27177,7 @@ var _reactDefault = parcelHelpers.interopDefault(_react);
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
- */ var _motionBackgroundJpg = require("../images/motion-background.jpg");
+ **/ var _motionBackgroundJpg = require("../images/motion-background.jpg");
 var _motionBackgroundJpgDefault = parcelHelpers.interopDefault(_motionBackgroundJpg);
 const imageAltText = "purple and blue abstract background";
 /**
